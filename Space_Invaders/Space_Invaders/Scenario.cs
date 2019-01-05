46c28c8a5387734c68ac9a97f44095acf93a5bcc
Hugo Martins <33464471+DaPikachuOnMeth@users.noreply.github.com>
2019-01-05 18:55:27 +0000

    minor changes

--- Space_Invaders/Space_Invaders/Scenario.cs
+++ Space_Invaders/Space_Invaders/Scenario.cs
@@ -9,17 +9,13 @@ namespace Space_Invaders
     class Scenario
     {
         //classe responsável pelo cenário do jogo
-        private readonly int hiScore;
         internal int score;
         internal int lives;
-        internal string[,] enemies;
 
         public IEntity[,] entities;
 
         public void Update()
         {
-            Console.Clear();
-            Draw_Hud();
         }
 
 
@@ -42,25 +38,7 @@ namespace Space_Invaders
         }
 
 
-        public void Draw_Hud()
-        {
-            Console.WriteLine("00000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
-            Console.WriteLine("00000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
-            Console.SetCursorPosition(2, 2);
-            Console.Write("Score " + score);
-
-            Console.SetCursorPosition(2, 32);
-            Console.Write("Hi-Score " + hiScore);
-
-
-            Console.SetCursorPosition(2, 68);
-            Console.Write("Lives " + lives);
-
-            Console.SetCursorPosition(0, 29);
-            Console.WriteLine("00000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
-            Console.SetCursorPosition(0, 30);
-            Console.WriteLine("00000000000000000000000000000000000000000000000000000000000000000000000000000000000000");
-        }
+        
 
         private void GetEnemie(string visual)
         {
