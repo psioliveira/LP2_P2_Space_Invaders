d9059cd2608353176d89d27fd56a991c6e257942
Pedro Oliveira <pedrooliveira2007@hotmail.com>
2019-01-06 02:59:35 +0000

    multiThreading started, gameLoop started

--- Space_Invaders/Space_Invaders/Program.cs
+++ Space_Invaders/Space_Invaders/Program.cs
@@ -4,17 +4,134 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Collections.Concurrent;
+using System.Threading;
 
 namespace Space_Invaders
 {
-    class Program
+    internal class Program
     {
-        //classe principal do jogo
-        static void Main(string[] args)
+
+        /// <summary>
+        /// Coleção thread-safe que vamos utilizar para passar as teclas lidas
+        /// do produtor para o consumidor.
+        /// </summary>
+        private readonly BlockingCollection<ConsoleKey> queue;
+
+        /// <summary>
+        /// Valor base de milisegundos por frame.
+        /// (17ms/frame = aprox. 60fps)
+        /// </summary>
+        private long MS_PER_FRAME = 17;
+
+
+        /// <summary>Inicio do Programa</summary>
+        static void Main()
+        {
+            Program prog = new Program();
+            prog.Start();
+        }
+
+        /// <summary>Constroi uma instância da classe Program.</summary>
+
+        public Program()
+        {
+            //  Instanciar a coleção thread-safe; por omissão esta coleção
+            // utiliza uma fila, mas isto pode ser alterado invocando outro
+            // overload do construtor
+            queue = new BlockingCollection<ConsoleKey>();
+        }
+
+        /// <summary>Inicia o programa propriamente dito.</summary>
+        private void Start()
+        {
+            // Declarar e instanciar thread produtora (coloca teclas na
+            // coleção) e thread consumidora (retira teclas da coleção)
+            Thread prodThread = new Thread(PlayerInput);
+
+            // Iniciar thread
+            prodThread.Start();
+
+            // Esperar que as threads terminem
+            prodThread.Join();
+        }
+
+
+        void GameLoop()
+        {
+            while (true)
+            {
+                long start = DateTime.Now.Ticks; // 10000 ticks = 1 segundo
+                // Iniciar threads
+                ColectInput();
+                Update();
+                Render();
+                Thread.Sleep(Convert.ToInt32((start / 10000) + MS_PER_FRAME - (DateTime.Now.Ticks / 10000)));
+            }
+        }
+
+        /// <summary>
+        /// Método que lê teclas do teclado e coloca-as na fila thread-safe.
+        /// </summary>
+        private void PlayerInput()
+        {
+            // Variável para as teclas pressionadas
+            ConsoleKey key;
+
+            // Ler teclas pressionadas enquanto o escape não for pressionado
+            do
+            {
+                // Lê tecla premida
+                key = Console.ReadKey(false).Key;
+
+                queue.Add(key);
+
+            } while (key != ConsoleKey.Escape);
+        }
+
+
+
+        private void ColectInput()
+        {
+            // Variável para as teclas retiradas da coleção
+            ConsoleKey key;
+
+            // Ler teclas pressionadas enquanto o escape não for pressionado
+            while (queue.TryTake(out key) || key == ConsoleKey.Escape)
+            {
+
+                if (key == ConsoleKey.W || key == ConsoleKey.UpArrow || key == ConsoleKey.Spacebar)
+                {
+                    Console.WriteLine("Tiro");
+                }
+
+                else if (key == ConsoleKey.D || key == ConsoleKey.RightArrow)
+                {
+                    Console.WriteLine("Direita");
+                }
+                else if (key == ConsoleKey.A || key == ConsoleKey.LeftArrow)
+                {
+                    Console.WriteLine("Esquerda");
+                }
+
+            }
+        }
+
+
+        private void Update()
         {
 
-
-            Console.ReadKey();
         }
+
+
+        public void Render() { }
+
     }
+
+
+
+
+
 }
+
+
