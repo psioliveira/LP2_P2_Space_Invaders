aea7db3eaf0bb76926c37ff9a93c24bc66345293
Pedro Oliveira <pedrooliveira2007@hotmail.com>
2019-01-06 17:13:01 +0000

    start scenatio methods added

--- Space_Invaders/Space_Invaders/Scenario.cs
+++ Space_Invaders/Space_Invaders/Scenario.cs
@@ -11,7 +11,7 @@ namespace Space_Invaders
         //classe responsável pelo cenário do jogo
         internal int score;
         internal int lives;
-        internal Slot[,] gameWorld = new Slot[28, 87];
+        internal Slot[,] gameWorld = new Slot[26, 87];
         public IEntity[] entities = new IEntity[174];
 
         public void Update()
@@ -35,8 +35,8 @@ namespace Space_Invaders
             bool player = false; //id 0
             for (uint i = 0; i < entities.Length; i++)
             {
-                if (i == 0 && !player)
-                {
+                if (i == 0 && !player) //cria o player
+                {//id = 0
                     player = true;
                     entities[i] = new Player();
                     continue;
@@ -46,7 +46,7 @@ namespace Space_Invaders
                 if (enemies < 60)
                 {
                     if (enemies == 0) //cria o ovni
-                    {
+                    {//id = 1
                         entities[i] = new Enemy(4, i);
                         enemies++;
                         continue;
@@ -73,8 +73,8 @@ namespace Space_Invaders
                     }
                 }
 
-                if (neutrals < 112) //cria os blocos dos esudos
-                {
+                if (neutrals < 112) //cria os blocos dos escudos
+                { //id 64 ao 178
                     entities[i] = new Neutral(i);
                 }
 
@@ -84,16 +84,69 @@ namespace Space_Invaders
 
         private void PlaceEntities()
         {
+            int c = 0;
+            int[] barrierPos = new int[] { 20, 5 };
             foreach (IEntity e in entities)
             {
+
+
+                //player
                 if (e.ID == 0)
                 {
                     SetVisual(e);
                 }
 
-                if (e.ID)
+                //minions tier 1
+                if (e.ID > 1 && e.ID < 14)
+                {
+                    e.SCoorY = 6;
+
+                    for (int i = 0; i < e.SlotNumber; i++)
+                    {
+                        e.SCoorX = Convert.ToUInt32(c + i);
+                        gameWorld[e.SCoorY, e.SCoorX].owner = e;
+                    }
+                    if (e.ID < 13) { c += 6; }
+                    else { c = 0; }
+                }
 
 
+                //minions tier 2
+                if (e.ID > 13 && e.ID < 39)
+                {
+                    if (e.ID < 26) { e.SCoorY = 8; }
+                    else { e.SCoorY = 10; }
+
+                    for (int i = 0; i < e.SlotNumber; i++)
+                    {
+                        e.SCoorX = Convert.ToUInt32(c + i);
+                        gameWorld[e.SCoorY, e.SCoorX].owner = e;
+                    }
+                    if (e.ID > 37 || e.ID == 25) { c = 0; }
+                    else { c += 6; }
+                }
+
+
+                //minions tier 3
+                if (e.ID > 38 && e.ID < 64)
+                {
+                    if (e.ID < 51) { e.SCoorY = 12; }
+                    else { e.SCoorY = 14; }
+
+                    for (int i = 0; i < e.SlotNumber; i++)
+                    {
+                        e.SCoorX = Convert.ToUInt32(c + i);
+                        gameWorld[e.SCoorY, e.SCoorX].owner = e;
+                    }
+                    if (e.ID > 62 || e.ID == 50) { c = 0; }
+                    else { c += 6; }
+                }
+
+                //barreiras
+                if (e.ID > 63 && e.ID <= 178)
+                {
+                    barrierPos = BuildBarrier(e, barrierPos);
+                }
             }
         }
 
@@ -104,6 +157,80 @@ namespace Space_Invaders
                 gameWorld[entity.SCoorY, entity.SCoorX + i].owner = entity;
             }
         }
+
+        private int[] BuildBarrier(IEntity e, int[] lastPos)
+        {
+            int[] position = new int[] { 0, 0 };
+            e.SCoorX = Convert.ToUInt32(lastPos[1]);
+            e.SCoorY = Convert.ToUInt32(lastPos[0]);
+            gameWorld[lastPos[0], lastPos[1]].owner = e;
+
+            if (lastPos[0] == 20)
+            {
+                position[0] = lastPos[0];
+                if (lastPos[1] == 10 || lastPos[1] == 34 || lastPos[1] == 56)
+                { position[1] = lastPos[1] + 19; }
+
+                if (lastPos[1] == 80)
+                {
+                    position[0]++;
+                    position[1] = 4;
+                }
+            }
+
+
+            if (lastPos[0] == 21)
+            {
+                position[0] = lastPos[0];
+                if (lastPos[1] == 11 || lastPos[1] == 35 || lastPos[1] == 57)
+                { position[1] = lastPos[1] + 17; }
+
+                if (lastPos[1] == 81)
+                {
+                    position[0]++;
+                    position[1] = 4;
+                }
+            }
+
+            if (lastPos[0] == 22)
+            {
+                position[0] = lastPos[0];
+
+                if (lastPos[1] == 6 || lastPos[1] == 30 || lastPos[1] == 52 || lastPos[1] == 76)
+                { position[1] = lastPos[1] + 3; }
+
+                if (lastPos[1] == 11 || lastPos[1] == 35 || lastPos[1] == 57)
+                { position[1] = lastPos[1] + 17; }
+
+                if (lastPos[1] == 81)
+                {
+                    position[0]++;
+                    position[1] = 4;
+                }
+            }
+
+            if (lastPos[0] == 23 || lastPos[0] == 24)
+            {
+                position[0] = lastPos[0];
+
+                if (lastPos[1] == 5 || lastPos[1] == 29 || lastPos[1] == 51 || lastPos[1] == 75)
+                { position[1] = lastPos[1] + 5; }
+
+                if (lastPos[1] == 11 || lastPos[1] == 35 || lastPos[1] == 57)
+                { position[1] = lastPos[1] + 17; }
+
+                if (lastPos[1] == 81)
+                {
+                    position[0]++;
+                    position[1] = 4;
+                }
+            }
+
+
+            return position;
+        }
+
+
     }
 }
 
