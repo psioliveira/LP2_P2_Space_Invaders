f21895aa4af5e5d633255bbb9fee2a45bc54e2c7
pH-Keith <33523999+pH-Keith@users.noreply.github.com>
2019-01-06 02:54:30 +0000

    Neutral and slot classes added

---
+++ Space_Invaders/Space_Invaders/Slot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_Invaders
+{
+    class Slot
+    {
+
+        internal char c = '.';
+        internal IEntity isPartOf = null;
+
+        public Slot() { }
+
+    }
+}
